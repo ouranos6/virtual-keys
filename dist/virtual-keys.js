19c646bffc323f6bf420b6439ab0dc490eb448ff
ouranos6 <morgade.ivan@gmail.com>
2023-04-28 14:37:40 +0200

    Update virtual-keys.js

--- dist/virtual-keys.js
+++ dist/virtual-keys.js
@@ -102,6 +102,10 @@ class VirtualKeysPanel extends LitElement {
         <g><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"></path></g>
       </svg>`;
   }
+  
+  qrButton() {
+    return html`<svg fill="#000000" width="24px" height="24px" viewBox="0 0 24 24" id="qr-code" data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" class="icon flat-line"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M9,10H4A1,1,0,0,1,3,9V4A1,1,0,0,1,4,3H9a1,1,0,0,1,1,1V9A1,1,0,0,1,9,10ZM21,9V4a1,1,0,0,0-1-1H15a1,1,0,0,0-1,1V9a1,1,0,0,0,1,1h5A1,1,0,0,0,21,9ZM10,20V15a1,1,0,0,0-1-1H4a1,1,0,0,0-1,1v5a1,1,0,0,0,1,1H9A1,1,0,0,0,10,20Z" style="fill: #2ca9bc; stroke-width: 2;"></path><path id="primary" d="M21,14v5a2,2,0,0,1-2,2H14" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-2" data-name="primary" d="M17,17H14V14h3ZM10,9V4A1,1,0,0,0,9,3H4A1,1,0,0,0,3,4V9a1,1,0,0,0,1,1H9A1,1,0,0,0,10,9Zm10,1H15a1,1,0,0,1-1-1V4a1,1,0,0,1,1-1h5a1,1,0,0,1,1,1V9A1,1,0,0,1,20,10ZM9,21H4a1,1,0,0,1-1-1V15a1,1,0,0,1,1-1H9a1,1,0,0,1,1,1v5A1,1,0,0,1,9,21Z" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>`;
+  }
 
   showAlert(text) {
     this.alert = text;
@@ -163,6 +167,7 @@ class VirtualKeysPanel extends LitElement {
                 <mwc-list-item hasMeta twoline @click=${e => this.listItemClick(e, token)}>
                   <span> ${token.name} Link ->  </span><a href=${this.hass.hassUrl() + 'local/community/virtual-keys/login.html?token=' + token.jwt_token}>Link</a>
                   <span slot="secondary">${token.user}, Expire: ${humanSeconds(token.remaining)}</span>
+                  <mwc-icon slot="meta" @click=${e => this.qrClick(e, token)}>${this.qrButton()}</mwc-icon>
                   <mwc-icon slot="meta" @click=${e => this.deleteClick(e, token)}>${this.deleteButton()}</mwc-icon>
                 </mwc-list-item>
               `)}
