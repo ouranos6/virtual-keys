6e2742ab724f306767e93ca363efa8e879e1dfa7
ouranos6 <20718760+ouranos6@users.noreply.github.com>
2024-06-17 11:26:33 +0200

    Update virtual-keys.js

--- dist/virtual-keys.js
+++ dist/virtual-keys.js
@@ -15,6 +15,34 @@ function humanSeconds(seconds) {
     return value > 0 ? `${value} ${label}${value !== 1 ? 's' : ''} ` : '';
   }).join(' ');
 }
+let stopAdd = false;
+let map1 = new Map()
+
+function addApi() {
+  
+  if(stopAdd == false){
+    // Address of the current window
+    let address = window.location.search
+  
+    // Returns a URLSearchParams object instance
+    let parameterList = new URLSearchParams(address)
+  
+    // Created a map which holds key value pairs
+    
+  
+    // Storing every key value pair in the map
+    parameterList.forEach((value, key) => {
+        map1.set(key, value)
+    })
+    stopAdd = true;
+    return map1;
+  } else {
+    let map1 = new Map()
+    return map1;
+  }
+  
+  
+}
 
 class VirtualKeysPanel extends LitElement {
   static get properties() {
@@ -34,11 +62,14 @@ class VirtualKeysPanel extends LitElement {
     this.users = [];
     this.tokens = [];
     this.alert = '';
-
+    this.userId = '';
+    addApi();
+    
     // form inputs
     this.name = '';
     this.user = '';
-    this.expire = 0;
+    this.expire = 60;
+    this.stopAddClick = false;
   }
 
   fetchUsers() {
@@ -66,8 +97,14 @@ class VirtualKeysPanel extends LitElement {
   }
 
   update(changedProperties) {
+    
     if (changedProperties.has('hass') && this.hass) {
       this.fetchUsers();
+      if (this.stopAddClick == false) {
+        this.addClick();
+        console.log("ok");
+      }
+      
     }
     super.update(changedProperties);
   }
@@ -84,17 +121,40 @@ class VirtualKeysPanel extends LitElement {
     this.expire = e.target.value;
   }
 
+  toggleSideBar() {
+    this.dispatchEvent(new Event('hass-toggle-menu', { bubbles: true, composed: true}));
+  }
   addClick() {
-    this.hass.callWS({
-      type: 'virtual_keys/create_token',
-      name: this.name,
-      user_id: this.user,
-      minutes: parseInt(this.expire, 10),
-    }).then(() => {
-      this.fetchUsers();
-    }).catch(err => {
-      this.showAlert(err.message);
-    });
+    this.stopAddClick = true;
+    if (map1.size != 0) {
+      this.hass.callWS({
+        type: 'virtual_keys/create_token',
+        name: map1.get("n"),
+        user_id: map1.get("u"),
+        minutes: parseInt(map1.get("m"), 10),
+      }).then(() => {
+        this.fetchUsers();
+      }).catch(err => {
+        this.showAlert(err.message);
+      });
+
+    } else {
+      this.hass.callWS({
+        type: 'virtual_keys/create_token',
+        name: this.name,
+        user_id: this.user,
+        minutes: parseInt(this.expire, 10),
+      }).then(() => {
+        this.fetchUsers();
+      }).catch(err => {
+        this.showAlert(err.message);
+      });
+    }
+    const baseUrl = this.hass.hassUrl() + 'local/community/virtual-keys/login.html?token=' + token.jwt_token;
+    const tokenQrUrl = this.hass.hassUrl() + 'local/community/virtual-keys/qr.html?qr=' + baseUrl + '&r=' + token.user;
+    console.log(tokenQrUrl);
+
+    window.open(tokenQrUrl, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=200,width=560,height=680");
   }
 
   deleteButton() {
@@ -102,9 +162,8 @@ class VirtualKeysPanel extends LitElement {
         <g><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"></path></g>
       </svg>`;
   }
-  
   qrButton() {
-    return html`<svg fill="#000000" width="24px" height="24px" viewBox="0 0 24 24" id="qr-code" data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" class="icon flat-line"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M9,10H4A1,1,0,0,1,3,9V4A1,1,0,0,1,4,3H9a1,1,0,0,1,1,1V9A1,1,0,0,1,9,10ZM21,9V4a1,1,0,0,0-1-1H15a1,1,0,0,0-1,1V9a1,1,0,0,0,1,1h5A1,1,0,0,0,21,9ZM10,20V15a1,1,0,0,0-1-1H4a1,1,0,0,0-1,1v5a1,1,0,0,0,1,1H9A1,1,0,0,0,10,20Z" style="fill: #2ca9bc; stroke-width: 2;"></path><path id="primary" d="M21,14v5a2,2,0,0,1-2,2H14" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-2" data-name="primary" d="M17,17H14V14h3ZM10,9V4A1,1,0,0,0,9,3H4A1,1,0,0,0,3,4V9a1,1,0,0,0,1,1H9A1,1,0,0,0,10,9Zm10,1H15a1,1,0,0,1-1-1V4a1,1,0,0,1,1-1h5a1,1,0,0,1,1,1V9A1,1,0,0,1,20,10ZM9,21H4a1,1,0,0,1-1-1V15a1,1,0,0,1,1-1H9a1,1,0,0,1,1,1v5A1,1,0,0,1,9,21Z" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>`;
+    return html`<svg fill="#000000" width="24px" height="24px" viewBox="0 0 24 24" id="qr-code" data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" class="icon flat-line"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M9,10H4A1,1,0,0,1,3,9V4A1,1,0,0,1,4,3H9a1,1,0,0,1,1,1V9A1,1,0,0,1,9,10ZM21,9V4a1,1,0,0,0-1-1H15a1,1,0,0,0-1,1V9a1,1,0,0,0,1,1h5A1,1,0,0,0,21,9ZM10,20V15a1,1,0,0,0-1-1H4a1,1,0,0,0-1,1v5a1,1,0,0,0,1,1H9A1,1,0,0,0,10,20Z" style="fill: #BDBD10; stroke-width: 2;"></path><path id="primary" d="M21,14v5a2,2,0,0,1-2,2H14" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-2" data-name="primary" d="M17,17H14V14h3ZM10,9V4A1,1,0,0,0,9,3H4A1,1,0,0,0,3,4V9a1,1,0,0,0,1,1H9A1,1,0,0,0,10,9Zm10,1H15a1,1,0,0,1-1-1V4a1,1,0,0,1,1-1h5a1,1,0,0,1,1,1V9A1,1,0,0,1,20,10ZM9,21H4a1,1,0,0,1-1-1V15a1,1,0,0,1,1-1H9a1,1,0,0,1,1,1v5A1,1,0,0,1,9,21Z" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>`;
   }
 
   showAlert(text) {
@@ -126,33 +185,52 @@ class VirtualKeysPanel extends LitElement {
       this.showAlert(err.message);
     });
   }
-  
+
   qrClick(e, token) {
     e.stopPropagation();
     const baseUrl = this.hass.hassUrl() + 'local/community/virtual-keys/login.html?token=' + token.jwt_token;
     const tokenQrUrl = this.hass.hassUrl() + 'local/community/virtual-keys/qr.html?qr=' + baseUrl + '&r=' + token.user;
+    console.log(tokenQrUrl);
 
     window.open(tokenQrUrl, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=200,width=560,height=680");
   }
 
+  getLoginUrl(token) {
+    return this.hass.hassUrl() + 'local/community/virtual-keys/login.html?token=' + token.jwt_token;
+  }
+
   listItemClick(e, token) {
-    const tokenLoginUrl = this.hass.hassUrl() + 'local/community/virtual-keys/login.html?token=' + token.jwt_token;
-    navigator.clipboard.writeText(tokenLoginUrl);
+    navigator.clipboard.writeText(this.getLoginUrl(token));
     this.showAlert('Copied to clipboard ' + token.name);
   }
 
   render() {
     return html`
-      <ha-app-layout>
-        <app-header slot="header" fixed>
-          <app-toolbar>
-            <div main-title>${this.panel.title}</div>
-          </app-toolbar>
-        </app-header>
+      <div>
+        <header class="mdc-top-app-bar mdc-top-app-bar--fixed">
+          <div class="mdc-top-app-bar__row">
+            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start" id="navigation">
+              <div>
+                <mwc-icon-button title="Sidebar Toggle" @click=${this.toggleSideBar}>
+                  <svg preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+                    <g><path class="primary-path" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"></path></g>
+                  </svg>
+                </mwc-icon-button>
+              </div>
 
-        <div class="flex content">
+              <span class="mdc-top-app-bar__title">
+                ${this.panel.title}
+              </span>
+            </section>
+            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" id="actions" role="toolbar">
+              <slot name="actionItems"></slot>
+            </section>
+          </div>
+        </header>
+
+        <div class="mdc-top-app-bar--fixed-adjust flex content">
           <div class="filters">
-            <paper-input label="Key name" value="" @value-changed=${this.nameChanged} .error-message=${'aa'}></paper-input>
+            <ha-textfield label="Key name" value="" @input="${this.nameChanged}"></ha-textfield>
 
             <ha-combo-box
               .items=${this.users}
@@ -164,11 +242,10 @@ class VirtualKeysPanel extends LitElement {
             >
             </ha-combo-box>
 
-            <paper-input label="Expire (minutes)" type="number" value="60" @value-changed=${this.expireChanged}></paper-input>
+            <ha-textfield label="Expire (minutes)" type="number" value="${this.expire}"" @input="${this.expireChanged}"></ha-textfield>
 
-            <mwc-button raised label="Add" @click=${this.addClick}></mwc-button>
+            <mwc-button raised label="Add Key" @click=${this.addClick}></mwc-button>
           </div>
-
           <ha-card>
             <mwc-list>
               ${this.tokens.map(token => html`
@@ -185,13 +262,69 @@ class VirtualKeysPanel extends LitElement {
 
       ${this.alert.length ? html`<ha-alert>${this.alert}</ha-alert>` : ''}
 
-      </ha-app-layout>
+      </div>
     `;
   }
 
   static get styles() {
     return css`
       :host {
+      }
+      .mdc-top-app-bar {
+        --mdc-typography-headline6-font-weight: 400;
+        color: var(--app-header-text-color,var(--mdc-theme-on-primary,#fff));
+        background-color: var(--app-header-background-color,var(--mdc-theme-primary));
+        width: var(--mdc-top-app-bar-width,100%);
+        display: flex;
+        position: fixed;
+        flex-direction: column;
+        justify-content: space-between;
+        box-sizing: border-box;
+        width: 100%;
+        z-index: 4;
+      }
+      .mdc-top-app-bar--fixed {
+        transition: box-shadow 0.2s linear 0s;
+      }
+      .mdc-top-app-bar--fixed-adjust {
+        padding-top: var(--header-height);
+      }
+      .mdc-top-app-bar__row {
+        height: var(--header-height);
+        border-bottom: var(--app-header-border-bottom);
+        display: flex;
+        position: relative;
+        box-sizing: border-box;
+        width: 100%;
+        height: 64px;
+      }
+      .mdc-top-app-bar__section--align-start {
+        justify-content: flex-start;
+        order: -1;
+      }
+      .mdc-top-app-bar__section {
+        display: inline-flex;
+        flex: 1 1 auto;
+        align-items: center;
+        min-width: 0px;
+        padding: 8px 12px;
+        z-index: 1;
+      }
+      .mdc-top-app-bar__title {
+        -webkit-font-smoothing: antialiased;
+        font-family: var(--mdc-typography-headline6-font-family,var(--mdc-typography-font-family,Roboto,sans-serif));
+        font-size: var(--mdc-typography-headline6-font-size,1.25rem);
+        line-height: var(--mdc-typography-headline6-line-height,2rem);
+        font-weight: var(--mdc-typography-headline6-font-weight,500);
+        letter-spacing: var(--mdc-typography-headline6-letter-spacing,.0125em);
+        text-decoration: var(--mdc-typography-headline6-text-decoration,inherit);
+        text-transform: var(--mdc-typography-headline6-text-transform,inherit);
+        padding-left: 20px;
+        padding-right: 0px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        z-index: 1;
       }
       app-header {
         background-color: var(--primary-color);
@@ -221,11 +354,16 @@ class VirtualKeysPanel extends LitElement {
         align-items: flex-end;
         display: flex;
         flex-wrap: wrap;
-        padding: 8px 16px 0px;
+        padding: 50px 16px 0px;
       }
       .filters > * {
         margin-right: 8px;
       }
+      @media (min-width: 870px) {
+        mwc-icon-button {
+          display: none;
+        }
+      }	   
     `;
   }
 }
